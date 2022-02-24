d4e3eed1436721b021f07905a2835adcbb2b9725
Duane McKibbin <duane@entelect.co.za>
2022-02-24 16:32:31 +0200

    Password hash support

--- ezviz.net/EzvizClient.cs
+++ ezviz.net/EzvizClient.cs
@@ -1,8 +1,10 @@
 ﻿using ezviz.net.domain;
 using Refit;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace ezviz.net;
-public class EzvizClient : IDisposable
+public class EzvizClient 
 {
     private const string DEFAULT_REGION = "apiieu.ezvizlife.com";
 
@@ -11,22 +13,24 @@ public class EzvizClient : IDisposable
     private readonly string region;
 
     public EzvizClient(string username, string password) : this(username, password, null)
-    {
-        this.username = username;
-        this.password = password;
-        this.region = region ?? DEFAULT_REGION;
+    {                
     }
 
     public EzvizClient(string username, string password, string? region)
     {
         this.username = username;
-        this.password = password;
+        this.password = GetPasswordHash(password);
         this.region = region ?? DEFAULT_REGION;
     }
 
-    public void Dispose()
+    private string GetPasswordHash(string plaintext)
     {
-     
+        using (var md5 = HashAlgorithm.Create("MD5"))
+        {
+            var bytes = Encoding.UTF8.GetBytes(plaintext);
+            var hashed = md5.ComputeHash(bytes);
+            return BitConverter.ToString(hashed).Replace("-", string.Empty).ToLower();            
+        }
     }
     
     public async Task<EzvizUser> Login()
@@ -43,4 +47,5 @@ public class EzvizClient : IDisposable
         var response = await azviewApi.Login(payload);
         return response.LoginUser;
     }
+
 }
