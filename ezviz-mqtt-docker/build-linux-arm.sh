f75661a5324b11ad2004c4613987ce4ae04d13c3
Duane McKibbin <duane@entelect.co.za>
2023-05-29 17:32:28 +0200

    Removed dedicated Docker project, not needed anymore

--- ezviz-mqtt-docker/build-linux-arm.sh
+++
@@ -1,2 +0,0 @@
-dotnet publish -c Release -r linux-arm64 --self-contained /p:DebugType=None /p:DebugSymbols=false
-#dotnet publish -c Release /p:DebugType=None /p:DebugSymbols=false /p:PublishSingleFile=false
